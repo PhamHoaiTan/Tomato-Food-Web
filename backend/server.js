fbb708d4db1e33d9b82e6d5793401e26e1e7f8b7
David Pham <95225335+PhamHoaiTan@users.noreply.github.com>
2025-06-06 13:52:23 +0700

    commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import foodRoute from './routes/foodRoute.js';
+import { connectDB } from './config/db.js';
+import cors from "cors";
+import userRoute from './routes/userRoute.js';
+import 'dotenv/config'
+import cartRoute from './routes/cartRoute.js';
+import orderRoute from './routes/orderRoute.js';
+const app = express();
+
+const PORT = 4000;
+
+app.use("/images", express.static("uploads"))
+app.use(cors());
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+connectDB()
+app.use("/api/food", foodRoute)
+app.use("/api/user", userRoute)
+app.use("/api/cart",cartRoute)
+app.use("/api/order",orderRoute)
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`)
+})
